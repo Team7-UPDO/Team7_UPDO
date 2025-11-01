c8ce886d126898849fb7ae36175e4f7d6587fa56
HBA <goddamncool@naver.com>
2025-11-01 23:04:13 +0900

    🐛 [FIX] #232 접근성 개선사항 추가 수정 (랜드마크)

--- src/app/gathering/page.tsx
+++ src/app/gathering/page.tsx
@@ -25,9 +25,9 @@ export default function GatheringPage() {
           />
         </div>
       </header>
-      <section>
+      <main>
         <GatheringSection />
-      </section>
+      </main>
     </>
   );
 }
